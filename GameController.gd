6a94a5281c069734613b9ed4be5c6db3e21d927d
kelbg <kelvyn.bg@outlook.com>
2024-08-06 00:46:53 -0300

    refactor: endpoint setup and request params

--- GameController.gd
+++ GameController.gd
@@ -4,34 +4,43 @@ extends Node
 
 signal message_received(char_name: String, msg: String)
 
-var url: String
-var headers: Array
 var messages: Array
+var current_endpoint: Dictionary
 
 func _ready() -> void:
 	$HTTPRequest.request_completed.connect(_on_request_completed)
-	setup_endpoint("OpenAI")
+	current_endpoint = setup_endpoint("OpenAI")
 	load_system_prompt("character.txt")
 
 func load_system_prompt(file_path: String) -> void:
 	var file: FileAccess = FileAccess.open(file_path, FileAccess.READ)
 	messages.append({"role": "system", "content": file.get_as_text()})
 
-func setup_endpoint(endpoint: String) -> void:
+func setup_endpoint(service: String) -> Dictionary:
+	var new_endpoint: Dictionary = JSON.parse_string(FileAccess.open("endpoints.json", FileAccess.READ).get_as_text())[service]
+	new_endpoint["headers"] += [
+		"Content-Type: application/json",
+		"Authorization: Bearer %s" % get_api_key(service)
+	]
+	
+	return new_endpoint
+
+func get_api_key(service: String) -> String:
 	var cfg: ConfigFile = ConfigFile.new()
 	cfg.load("environment.cfg")
-	var endpoints: Dictionary = JSON.parse_string(FileAccess.open("endpoints.json", FileAccess.READ).get_as_text())
-	url = endpoints[endpoint]["url"]
-	headers = ["Content-Type: application/json", "Authorization: Bearer %s" % cfg.get_value("", "%s_API_Key" % endpoint)]
+	return cfg.get_value("", "%s_API_KEY" % service)
 
 func send_request(role: String, content: String, model: String) -> void:
 	messages.append({"role": role, "content": content})
-	$HTTPRequest.request(url, headers, HTTPClient.METHOD_POST, JSON.stringify({
-		"model": model,
-		"temperature": 0.7,
-		"max_tokens": 200,
-		"messages": messages
-	}))
+	current_endpoint["params"]["model"] = model
+	current_endpoint["params"]["messages"] = messages
+	
+	$HTTPRequest.request(
+		current_endpoint["url"],
+		current_endpoint["headers"],
+		HTTPClient.METHOD_POST,
+		str(current_endpoint["params"])
+	)
 
 func _on_request_completed(_result: int, _response_code: int, _headers: PackedStringArray, body: PackedByteArray) -> void:
 	var json: Dictionary = JSON.parse_string(body.get_string_from_utf8())
